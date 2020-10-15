b0d54112f3caf4228d3c760db2de290dea9ffb0d
jeffryafandi <jeffryafandi1@gmail.com>
2020-10-15 15:43:38 +0700

    db

--- util.js
+++ util.js
@@ -5,9 +5,9 @@ class Util {
   static randomRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  static async db(id){
+  static async db(){
     const player = require("./models/web.js");
-    return await player.findOne({id: id}, (err, gg) => {
+    return await player.findOne({id: "web"}, (err, gg) => {
       if (err) return console.error(err)
     })
   }
