b124a9f2b42a4771e213af21666ac34b5a8bd1d1
jeffryafandi <jeffryafandi1@gmail.com>
2020-10-16 13:27:28 +0700

    tegram test

--- node/telegram.js
+++ node/telegram.js
@@ -1,33 +1,8 @@
-/*const TelegramBot = require('node-telegram-bot-api');
+const TeleBot = require('telebot');
+const bot = new TeleBot("1249511986:AAHOFLw475kqwUciLA8mFr2nEo2SHJEFm3s");
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '1249511986:AAHOFLw475kqwUciLA8mFr2nEo2SHJEFm3s';
-
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true });
-
-// Matches "/echo [whatever]"
-const util = require("../util.js");
-util.db().then(async (db) => {
-bot.onText(/\/maintenance (.+)/, async (msg, match) => {
-  const chatId = msg.chat.id;
-if(!match){
-  bot.sendMessage(chatId, `maintenance: ${db.maintenance}`)
-}else
-
-  const resp = match[1]; // the captured "whatever"
- if (resp = "true") await db.updateOne({maintenance: true})
- else await db.updateOne({maintenance: false})
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+bot.on(['/start', '/hello'], async(msg) =>{ 
+   
+   const m = await msg.reply.text('Welcome!')
+  msg.reply.text(m)
 });
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
-});
-})*/
