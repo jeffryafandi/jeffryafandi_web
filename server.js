fe6552cd86f3bd7bf703f94c476642dd69374fcc
jeffryafandi <jeffryafandi1@gmail.com>
2021-01-07 20:17:06 +0700

    aa

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/web/index.html");
