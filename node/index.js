c812a016cc203dcfbb5b0850a6123ae1ed2bd883
jeffryafandi <jeffryafandi1@gmail.com>
2021-01-07 16:57:07 +0700

    tes

--- node/index.js
+++ node/index.js
@@ -33,7 +33,7 @@ const util = require("../util.js");
     res.render("lisensi.ejs")
   })
   app.get("/tugas/:id", async (req, res) => {
-    res.render(`tugas/${req.params.id}`) || res.render("404")
+    res.sendFile(`tugas/${req.params.id}`) || res.render("404")
   })
   /*
   app.get("/tugas/7", function(req, res) {
