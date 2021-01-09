56d74a72e58e4cd4bee1e4f2a7e4bd2e81c92e12
jeffryafandi <jeffryafandi1@gmail.com>
2021-01-10 06:16:35 +0700

    a

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { resolve } = require("path"),
       ServerListener = require("./serverListen"),
-      server = new ServerListener(process.env.PORT || 3000, resolve(__dirname, "src"));
+      server = new ServerListener(process.env.PORT || 3000, resolve(__dirname, "/src"));
       
       
 server.run()
