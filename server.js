5d0ca6952774f19134c0c8c96f19b2f82ee24bc8
jeffryafandi <jeffryafandi1@gmail.com>
2021-01-10 05:33:05 +0700

    aaa

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const ServerListener = require("./serverListen"),
       server = new ServerListener(process.env.PORT || 3000, resolve(__dirname, "src"));
       
       
-server.run();
+server.run()
