665e0bd259af1bb0352b7031ef2657df24ac941f
jeffryafandi <jeffryafandi1@gmail.com>
2021-01-10 18:49:34 +0700

    Versi 2.0

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
-const { resolve } = require("path"),
-      ServerListener = require("./serverListen"),
-      server = new ServerListener(process.env.PORT || 3000, resolve(__dirname, "/src"));
-      
-      
-server.run()
+const http = require("http"),
+      ejs = require('ejs'),
+      express = require("express"),
+      app = express(),
+      path = require("path"),
+      config = require("config.json");
+
+app.set("views", path.join(__dirname, "/views"));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static("public"));
+app.use('/public', express.static('public'));
+app.use(express.static(__dirname + '/public'));
+
+//Routing
+if(config.maintenance === true){
+  app.use(function(req, res) {
+    res.render("maintenance")
+  });
+}else {
+  
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.get("/post/2021/:id", async (req, res) => {
+  res.render(`/post/2021/${req.params.id}`) || res.render("404");
+})
+
+app.use(function(req, res) {
+  res.render("404")
+});
+
+}
+app.listen(process.env.PORT);
