a908b85f241e2722f78935cbaf911224cb8fc79d
jeffryafandi <jeffryafandi1@gmail.com>
2021-06-11 22:47:10 +0700

    a

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.get("/post/2021/:id", async (req, res) => {
   res.render(`post/2021/2/${req.params.id}`) ||
     res.render("404");
 });
-return app.get("/novel/because-i-like-you/:id", async (req, res) => {
-  res.render(`post/novel/because-i-like-you/${req.params.id}`) || res.render("404");
+return app.get("/novel/because-i-like-you/:im", async (req, res) => {
+  res.render(`post/novel/because-i-like-you/${req.params.im}`) || res.render("404");
 });
 
 app.get("/post/:year/:month/:id", async (req, res) => {
