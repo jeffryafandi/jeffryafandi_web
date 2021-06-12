ee073fa2f623bd5755dcb0196f82751d11b9f13f
jeffryafandi <jeffryafandi1@gmail.com>
2021-06-13 00:10:49 +0700

    test

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/post/2021/:id", async (req, res) => {
     res.render("404");
 });
 app.get("/novel/because-i-like-you/:im/", async (req, res) => {
-  if (!isNaN(req.params.im) && (req.params.im + 0) <= 17) return res.render('404');
+  if (req.params.im < 18) return res.render('404');
   else
     res.render(`post/novel/because-i-like-you/${req.params.im}`);
 });
