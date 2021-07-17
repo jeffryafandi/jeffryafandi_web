336784eab6e4d89425b09a232185f8c8f313e836
jeffryafandi <jeffryafandi1@gmail.com>
2021-07-17 18:43:47 +0700

    test

--- server.js
+++ server.js
@@ -37,10 +37,15 @@ app.get("/library/because-i-like-you-bab-:im", async (req, res) => {
     res.render(`library/because-i-like-you/${req.params.im}`);
 });
 
-app.get("/test", async (req, res) => {
+app.get("/ad1", async (req, res) => {
   res.render("test")
 })
-
+app.get("/ad2", async (req, res) => {
+  res.render("test1")
+})
+app.get("/ad3", async (req, res) => {
+  res.render("test2")
+})
 app.get("/library/:im", async (req, res) => {
     res.render(`library/${req.params.im}/001`);
 });
