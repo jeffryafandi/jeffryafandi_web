c908719efd9cf6976be4d2ef50fc7fc1b32980ad
jeffryafandi <jeffryafandi1@gmail.com>
2021-08-24 06:08:52 +0700

    tes

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get("/library/search", async (req, res) => {
     let fol = fs.readdirSync(__dirname + `/views/library/${folder}`).filter(x => x !== "0.md")
     fol.forEach(name => {
       const mat = matter.read(__dirname + `/views/library/${folder}/${name}`)
-      end.push({ name: { "title": mat.data.title, "description": mat.data.description, "author": mat.data.author, "date": mat.data.date, "image": mat.data.image }, "url": "https://fyy.my.id/library/" + folder + "/" + name.slice(0, -3) })
+      end.push({ "file": { "title": mat.data.title, "description": mat.data.description, "author": mat.data.author, "date": mat.data.date, "image": mat.data.image }, "url": "https://fyy.my.id/library/" + folder + "/" + name.slice(0, -3) })
     })
   });
   let end_s = end.filter(x => x.file.title.includes("Because")).map(x => x.file);
