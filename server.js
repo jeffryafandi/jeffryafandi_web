1d9cad1a084a7efbabe0e3e48919c75a7d912cd0
jeffryafandi <jeffryafandi1@gmail.com>
2021-09-06 23:48:01 +0700

    tes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require("http"),
   path = require("path"),
   config = require("./config.json"),
   fs = require('fs'),
-  matter = require('gray-matter'),
+  matter = require('gray-matter');
 var bodyParser = require("body-parser");
 
 //Function and other
